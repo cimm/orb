be2ecbc3efced03205f33e13da8a48b194f30d81
Simon Schoeters <hamfilter@gmail.com>
2011-02-20 00:22:45 +0100

    Help messages updated with weather and train parsers

--- computer/lib/argument_parser.rb
+++ computer/lib/argument_parser.rb
@@ -2,24 +2,57 @@ require 'ostruct'
 require 'optparse'
 
 class ArgumentParser
-  # TODO -p and -l are not yet REQUIRED
+  # TODO Find something for required arguments
   def self.parse(args)
     options = OpenStruct.new
-    opts = OptionParser.new do |opts|
-      opts.banner = "Usage: parser.rb -p PORT"
+    opts = case args.first
+      when "weather" then weather_arguments
+      when "delay" then delay_arguments
+      else main_arguments
+    end
+    opts.parse!(args)
+    unless args.include?("-h") || args.include?("--help")
+      puts opts
+    end
+    options
+  end
+
+  def self.main_arguments
+    OptionParser.new do |opts|
+      opts.banner = "Usage: ./listener PARSER [ARGS] [-h|--help]"
       opts.separator ""
-      opts.on("-p", "--port PORT", "Use PORT to connect with the Arduino bord") do |port|
+      opts.separator "Available commands:"
+      opts.separator "   weather\tWeather forecast for a given location"
+      opts.separator "   delay\tNext train delay for a given connection"
+    end
+  end
+
+  def self.weather_arguments
+    OptionParser.new do |opts|
+      opts.banner = "Usage: ./listener weather -p|--port PORT -l|--location WOID [-h|--help]"
+      opts.separator ""
+      opts.on("-p", "--port PORT", "The USB port used to connect with the Arduino bord") do |port|
         options.port = port
       end
       opts.on("-l", "--location YAHOO_WOEID", "Weather forecast at the given Yahoo! WOEID (eg. 966989 for Beauvechain)") do |location|
         options.location = location
       end
-      opts.on_tail("-h", "--help", "Show this message") do
-        puts opts
-        exit
+    end
+  end
+
+  def self.delay_arguments
+    OptionParser.new do |opts|
+      opts.banner = "Usage: ./listener delay -p|--port PORT -f|--from STATION -t|--to STATION [-h|--help]"
+      opts.separator ""
+      opts.on("-p", "--port PORT", "The USB port used to connect with the Arduino bord") do |port|
+        options.port = port
+      end
+      opts.on("-o", "--origin STATION", "Name of the Belgian station of orgin (eg. Leuven)") do |origin|
+        options.origin = origin
+      end
+      opts.on("-d", "--destination STATION", "Name of the Belgian destination station (eg. Kortrtijk)") do |destination|
+        options.destination = destination
       end
     end
-    opts.parse!(args)
-    options
   end
 end
